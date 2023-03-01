ef3a73d2f7cd3a84444905c464f09712457b95f9
Georg Oldenburger <georgoldenb@gmail.com>
2023-03-01 19:50:53 +0100

    WIP (Work In Progress) refactor printTime improved hours collection

--- functions/01-printTime.js
+++ functions/01-printTime.js
@@ -1,18 +1,25 @@
 const test = require('node:test');
 const assert = require('node:assert');
 
-/**
- * Write a function printTime
- * 0 hour - midnight
- * 12 hour - noon
- * 53-7 minute - o'clock
- * 8-23 minute - quarter past
- * 24-37 minute - half past
- * 38-52 minute - quarter to
- * @param{number} hour - 0-24 format, hour of a day
- * @param{number} minute - 0-59 format, minute of a day
- * @returns{string} returns a string representation of a time
- */
+const HOURS = [
+  /** 0 */ 'twelve',
+  /** 1 */ 'one',
+  /** 2 */ 'two',
+  /** 3 */ 'three',
+  /** 4 */ 'four',
+  /** 5 */ 'five',
+  /** 6 */ 'six',
+  /** 7 */ 'seven',
+  /** 8 */ 'eight',
+  /** 9 */ 'nine',
+  /** 10 */ 'ten',
+  /** 11 */ 'eleven',
+];
+
+const getTextHour = (hour) => {
+  const index = hour % 12;
+  return HOURS[index];
+}
 
 const hours = {
   1: 'one',
@@ -39,37 +46,50 @@ const hours = {
   22: 'ten',
   23: 'eleven',
   24: 'twelve',
-}
-function printTime( hour, minute ) {
+};
+
+/**
+ * Write a function printTime
+ * 0 hour - midnight
+ * 12 hour - noon
+ * 53-7 minute - o'clock
+ * 8-23 minute - quarter past
+ * 24-37 minute - half past
+ * 38-52 minute - quarter to
+ * @param{number} hour - 0-24 format, hour of a day
+ * @param{number} minute - 0-59 format, minute of a day
+ * @returns{string} returns a string representation of a time
+ */
+function printTime(hour, minute) {
   // validation of entered data
-  if ( hour > 24 || hour < 0 ) return false;
-  if ( minute > 60 || minute < 0 ) return false;
+  if (hour > 24 || hour < 0) return false;
+  if (minute > 60 || minute < 0) return false;
 
   //time rounding
   let roundedMinute;
-  if ( minute > 52 ) {
+  if (minute > 52) {
     roundedMinute = 0;
     hour += 1;
   }
-  if ( minute < 8 ) roundedMinute = 0;
-  if ( minute > 7  && minute < 24 ) roundedMinute = 15;
-  if ( minute > 23 && minute < 38 ) roundedMinute = 30;
-  if ( minute > 37 && minute < 53 ) roundedMinute = 45;
+  if (minute < 8) roundedMinute = 0;
+  if (minute > 7 && minute < 24) roundedMinute = 15;
+  if (minute > 23 && minute < 38) roundedMinute = 30;
+  if (minute > 37 && minute < 53) roundedMinute = 45;
 
   //result output
-  if ( hour === 24 || hour === 0 && roundedMinute === 0 ) return ( 'midnight' );
-  if ( hour === 12 && roundedMinute === 0 ) return ( 'noon' );
-  if ( roundedMinute === 0 ) return ( hours[hour] + ' o\'clock' );
-  if ( roundedMinute === 15 ) return ( 'quarter past ' + hours[hour] );
-  if ( roundedMinute === 30 ) return ( 'half past ' + hours[hour] );
-  if ( roundedMinute === 45 ) return ( 'quarter to ' + hours[hour + 1] );
+  if (hour === 24 || (hour === 0 && roundedMinute === 0)) return 'midnight';
+  if (hour === 12 && roundedMinute === 0) return 'noon';
+  if (roundedMinute === 0) return hours[hour] + " o'clock";
+  if (roundedMinute === 15) return 'quarter past ' + hours[hour];
+  if (roundedMinute === 30) return 'half past ' + hours[hour];
+  if (roundedMinute === 45) return 'quarter to ' + hours[hour + 1];
 
   // return `${hour}:${minute}`;
 }
 
 // Tests:
 test('printTime', () => {
-  assert.strictEqual(printTime(9, 0), 'nine o\'clock');
+  assert.strictEqual(printTime(9, 0), "nine o'clock");
   assert.strictEqual(printTime(9, 30), 'half past nine');
   assert.strictEqual(printTime(11, 53), 'noon');
   assert.strictEqual(printTime(12, 3), 'noon');
